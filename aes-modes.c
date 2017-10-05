34f46603ff5198dd3e859d13459be1242463f60a
Jackson Collier <jacksontcollier@gmail.com>
2017-10-05 19:03:42 -0400

    Add files for housing bulk of project logic

---
+++ aes-modes.c
@@ -0,0 +1,66 @@
+#include "aes-modes.h"
+
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *arg_flag_options = "k:i:o:v:";
+
+ArgFlags* new_ArgFlags()
+{
+  ArgFlags *arg_flags = (ArgFlags *) malloc(sizeof(ArgFlags));
+
+  arg_flags->key_file = NULL;
+  arg_flags->in_file = NULL;
+  arg_flags->out_file = NULL;
+  arg_flags->iv_file = NULL;
+
+  return arg_flags;
+}
+
+ArgFlags* parse_arg_flags(int argc, char * const argv[])
+{
+  int option;
+  ArgFlags *arg_flags;
+
+  arg_flags = new_ArgFlags();
+
+  while ((option = getopt(argc, argv, arg_flag_options)) != -1) {
+    switch(option) {
+      case 'k':
+        arg_flags->key_file = optarg;
+        break;
+      case 'i':
+        arg_flags->in_file = optarg;
+        break;
+      case 'o':
+        arg_flags->out_file = optarg;
+        break;
+      case 'v':
+        arg_flags->iv_file = optarg;
+        break;
+      default:
+        fprintf(stderr, "Unknown option\n");
+        exit(1);
+    }
+  }
+
+  return arg_flags;
+}
+
+void print_arg_flags(const ArgFlags *arg_flags)
+{
+  if (arg_flags->key_file != NULL) {
+    printf("Key file: %s\n", arg_flags->key_file);
+  }
+  if (arg_flags->in_file != NULL) {
+    printf("Input file: %s\n", arg_flags->in_file);
+  }
+  if (arg_flags->out_file != NULL) {
+    printf("Output file: %s\n", arg_flags->out_file);
+  }
+  if (arg_flags->iv_file != NULL) {
+    printf("IV file: %s\n", arg_flags->iv_file);
+  }
+}
+
---
+++ aes-modes.h
@@ -0,0 +1,13 @@
+typedef struct arg_flags
+{
+  char *key_file;
+  char *in_file;
+  char *out_file;
+  char *iv_file;
+} ArgFlags;
+
+ArgFlags* new_ArgFlags();
+
+ArgFlags* parse_arg_flags(int argc, char * const argv[]);
+
+void print_arg_flags(const ArgFlags *arg_flags);
