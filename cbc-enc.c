509dea134d82419e4d6879866ca27411ff3dd985
Jackson Collier <jacksontcollier@gmail.com>
2017-10-06 13:26:15 -0400

    Add logic for cbc-enc.c

--- cbc-enc.c
+++ cbc-enc.c
@@ -2,7 +2,20 @@
 
 int main(int argc, char **argv)
 {
-  ArgFlags *arg_flags = parse_arg_flags(argc, argv);
-  print_arg_flags(arg_flags);
+  ArgFlags *arg_flags;
+  AesKey* aes_key;
+  ByteBuf* cbc_plaintext;
+  ByteBuf* cbc_ciphertext;
+  ByteBuf* iv;
+
+  arg_flags = parse_arg_flags(argc, argv);
+  aes_key = get_aes_key(arg_flags->key_file);
+  cbc_plaintext = get_cbc_plaintext(arg_flags->in_file);
+  iv = get_iv(arg_flags->iv_file);
+
+  cbc_ciphertext = cbc_aes_encrypt(aes_key, cbc_plaintext, iv);
+
+  write_file(cbc_ciphertext, arg_flags->out_file);
+
   return 0;
 }
