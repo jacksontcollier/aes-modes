00d0abc9f640c6cd1a8fba4c25848e3d293c7ae6
Jackson Collier <jacksontcollier@gmail.com>
2017-10-06 20:22:11 -0400

    Add partial implementation of ctr-enc

--- ctr-enc.c
+++ ctr-enc.c
@@ -2,7 +2,18 @@
 
 int main(int argc, char **argv)
 {
-  ArgFlags *arg_flags = parse_arg_flags(argc, argv);
+  ArgFlags *arg_flags;
+  AesKey *aes_key;
+  ByteBuf *ctr_plaintext;
+  ByteBuf *ctr_ciphertext;
+  ByteBuf *iv;
+
+  arg_flags = parse_arg_flags(argc, argv);
+  aes_key = get_aes_key(arg_flags->key_file);
+  ctr_plaintext = read_file_contents(arg_flags->in_file);
+  iv = get_iv(arg_flags->iv_file);
+  ctr_ciphertext = ctr_aes_encrypt(aes_key, ctr_plaintext, iv);
+
   print_arg_flags(arg_flags);
   return 0;
 }
